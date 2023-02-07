1a07792d2d486ef61c7457f23003b48384785fa8
Putra B <cristminix@gmail.com>
2023-02-07 17:19:42 +0700

    ..

--- colab/init.sh
+++ colab/init.sh
@@ -1,6 +1,12 @@
 #/bin/bash
-
-mkdir -p ~/go/bin
+echo "mkdir -p ~/go/bin"
+!mkdir -p ~/go/bin
+!chmod +x ./cristminix.github.io/colab/bin/bore
+!cp -v ./cristminix.github.io/colab/bin/bore .
+!chmod +x ./cristminix.github.io/colab/bin/gotty
+!cp -v ./cristminix.github.io/colab/bin/gotty ~/go/bin/
+!chmod +x ./cristminix.github.io/colab/bin/gotty.sh
+!cp -v ./cristminix.github.io/colab/bin/gotty.sh .
 
 # add-apt-repository ppa:alessandro-strada/ppa
 # sudo apt update
