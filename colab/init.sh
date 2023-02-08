443e9659e218913e144359cb0c2c21e1a4ff96e7
Putra B <cristminix@gmail.com>
2023-02-08 12:42:47 +0700

    ...

--- colab/init.sh
+++ colab/init.sh
@@ -8,6 +8,14 @@ cp -v ./cristminix.github.io/colab/bin/gotty ~/go/bin/
 chmod +x ./cristminix.github.io/colab/bin/gotty.sh
 cp -v ./cristminix.github.io/colab/bin/gotty.sh .
 
+
+apt update
+apt install nginx
+
+cp -v ./cristminix.github.io/colab/config/nginx/default /etc/nginx/sites-available/default
+cp -rfv ./cristminix.github.io/colab/www .
+sudo service nginx restart
+
 # add-apt-repository ppa:alessandro-strada/ppa
 # sudo apt update
 # sudo apt install google-drive-ocamlfuse
