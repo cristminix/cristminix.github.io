a4464406874ff4effc8a69609f363a32e60caf74
Putra B <cristminix@gmail.com>
2023-02-09 15:07:25 +0700

    ...

--- frontend/src/views/themes/localtonet/HomePage.jsx
+++ frontend/src/views/themes/localtonet/HomePage.jsx
@@ -10,10 +10,10 @@ import SectionFour from './components/homepage/SectionFour'
 import SectionFive from './components/homepage/SectionFive'
 import SectionSix from './components/homepage/SectionSix'
 
-import "./assets/js/tiny-slider";
-import "./assets/js/feather.min";
-import "./assets/js/plugins.init";
-import "./assets/js/app";
+// import "./assets/js/tiny-slider";
+// import "./assets/js/feather.min";
+// import "./assets/js/plugins.init";
+// import "./assets/js/app";
 export default class HomePage extends React.Component{
 
     render(){
