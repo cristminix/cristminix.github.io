6f0a178db20abe03a14421bfa44fecf64987068b
Putra B <cristminix@gmail.com>
2023-02-13 13:33:29 +0700

    add ngrok setup-ngrok.sh

---
+++ backend/bin/setup-ngrok.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd /content
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+tar -xvf ngrok-v3-stable-linux-amd64.tgz
+./ngrok authtoken 2KRNVIf8yjdoth0L4hVPOC9VDbx_5hi5T3vy2N9xFcE77tWTu
