1ee0b949d128fa926a9d3365081edc872e4070dd
Putra B <cristminix@gmail.com>
2023-02-21 16:33:24 +0700

    ...

--- backend/bin/start-server-jail.sh
+++ backend/bin/start-server-jail.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 sudo service ssh start
 sudo service vsftpd start
 
-/opt/bin/gotty.sh & $SCRIPT_DIR/start-server.ssh --tunnel& bore local 25 --to=bore.pub
+/opt/bin/gotty.sh & $SCRIPT_DIR/start-server.sh --tunnel& bore local 25 --to=bore.pub
